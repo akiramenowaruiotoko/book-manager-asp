ad6821a42acc5a50f256e9241bf36977fe0e4df2
akiramenowaruiotoko <sawaking@hotmail.co.jp>
2024-01-31 21:51:49 +0900

    create table books

---
+++ db_setting/CREATE TABLE [dbo].[books].sql
@@ -0,0 +1,20 @@
+USE [book_manager]
+GO
+
+/****** Object:  Table [dbo].[books]    Script Date: 2024/01/31 21:48:17 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[books](
+	[book_id] [char](10) NOT NULL,
+	[book_name] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_books] PRIMARY KEY CLUSTERED 
+(
+	[book_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
