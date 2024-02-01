923a3f0d485bc7eccce07d1b9035518847ea2d15
akiramenowaruiotoko <sawaking@hotmail.co.jp>
2024-02-01 23:32:58 +0900

    Create CREATE TABLE [dbo].[Employees].sql

---
+++ db_setting/CREATE TABLE [dbo].[Employees].sql
@@ -0,0 +1,25 @@
+USE [book_manager]
+GO
+
+/****** Object:  Table [dbo].[employees]    Script Date: 2024/02/1 23:03:24 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[employees](
+	[employee_number] [char](10) NOT NULL,
+	[employee_password] [varchar](50) NOT NULL,
+	[first_name] [varchar](50) NULL,
+	[editor] [bit] NOT NULL,
+ CONSTRAINT [PK_employees] PRIMARY KEY CLUSTERED 
+(
+	[employee_number] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[employees] ADD  CONSTRAINT [DF_employees_editor]  DEFAULT ((0)) FOR [editor]
+GO
+
