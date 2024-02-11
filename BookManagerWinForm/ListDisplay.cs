3421be774c88b9d065c77602760d5d102451432a
akiramenowaruiotoko <sawaking@hotmail.co.jp>
2024-02-11 23:27:08 +0900

    add nuget sqlclient and sql connect1

--- BookManagerWinForm/ListDisplay.cs
+++ BookManagerWinForm/ListDisplay.cs
@@ -1,26 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace BookManagerWinForm
 {
     public partial class ListDisplay : Form
     {
+        private string connectionString;
+        private string viewName;
+
         public ListDisplay()
         {
             InitializeComponent();
+            // データベースマネージャーの初期化
+            connectionString = ConfigurationManager.ConnectionStrings["sqlsvr"].ConnectionString;
+            viewName = "View_all";
+        }
+
+        private void ListDisplay_Load(object sender, EventArgs e)
+        {
+            LoadDataFromView();
+        }
+
+        private void LoadDataFromView()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = $"SELECT * FROM {viewName}";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+
+                        // データをDataGridViewにバインド
+                        dataGridView1.DataSource = dataTable;
+                    }
+                }
+            }
         }
 
         private void ButtonBack_Click(object sender, EventArgs e)
         {
             this.Close();
         }
-
     }
 }
