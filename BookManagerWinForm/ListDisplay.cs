5c981ecd34ff1db02ebb27a3bc47ff6388562ba5
akiramenowaruiotoko <sawaking@hotmail.co.jp>
2024-02-25 13:26:33 +0900

    row button 4

--- BookManagerWinForm/ListDisplay.cs
+++ BookManagerWinForm/ListDisplay.cs
@@ -18,50 +18,31 @@ namespace BookManagerWinForm
             this.empNum = empNum;
             this.isEditor = isEditor;
             this.mainMenuForm = mainMenuForm;
-//            dbManager = new DatabaseManager(); // DatabaseManagerクラスのインスタンス化
- //           LoadDataFromView();
-
-            DataTable table = new DataTable();
-
-            DataColumn column1 = new DataColumn();
-            column1.ColumnName = "Field1";
-            column1.DataType = Type.GetType("System.Int64");
-            table.Columns.Add(column1);
-
-            DataColumn column2 = new DataColumn();
-            column2.ColumnName = "Field2";
-            column2.DataType = Type.GetType("System.String");
-            table.Columns.Add(column2);
-
-            dataGridView1.DataSource = table;
-
-            string[] str = { "ち", "は", "や", "ふ", "る" };
-
-            foreach (string t in str)
-            {
-                DataRow row = table.NewRow();
-                row[1] = t;
-                table.Rows.Add(row);
-            }
+            dbManager = new DatabaseManager(); // DatabaseManagerクラスのインスタンス化
+            LoadDataFromView();
         }
 
         private void LoadDataFromView()
         {
+            // NO列をボタン列に変更する
+            DataGridViewButtonColumn buttonColumn = new DataGridViewButtonColumn();
+            buttonColumn.HeaderText = "TEST";
+            buttonColumn.Name = "テスト";
+            buttonColumn.FlatStyle = FlatStyle.Popup;
+            buttonColumn.DataPropertyName = "Button";
+            this.dataGridView1.Columns.Add(buttonColumn);
+
             // データを取得する処理
             DataTable data = dbManager.GetDataFromView(viewName);
 
-            // NO列を追加
-            DataGridViewTextBoxColumn noColumn = new DataGridViewTextBoxColumn();
-            noColumn.Name = "NO";
-            noColumn.HeaderText = "NO";
-            dataGridView1.Columns.Insert(0, noColumn);
+            // NO列を追加し、ボタンを設定する
+            var button = new DataColumn("Button");
+            button.DataType = typeof(string);
+            data.Columns.Add(button);
 
-            // NO列に連番を表示するために、DataTableに列を追加し、連番を代入する
-            DataColumn noDataColumn = new DataColumn("NO", typeof(int));
-            data.Columns.Add(noDataColumn);
             for (int i = 0; i < data.Rows.Count; i++)
             {
-                data.Rows[i]["NO"] = i + 1;
+                data.Rows[i]["Button"] = "Click Here"+i;
             }
 
             // DataGridViewにデータをロード
@@ -74,13 +55,9 @@ namespace BookManagerWinForm
 
             // 行ヘッダーを表示して行番号を表示する
             dataGridView1.RowHeadersVisible = true;
-
-            // NO列と行番号に連番を表示する
-            for (int i = 0; i < dataGridView1.Rows.Count; i++)
-            {
-                dataGridView1.Rows[i].HeaderCell.Value = (i + 1).ToString();
-            }
+            dataGridView1.Visible = true;
         }
+
 
         private void ButtonBack_Click(object sender, EventArgs e)
         {
