6b7fe47fbecdb52ea55a58c6667e9c6da19084fa
akiramenowaruiotoko <sawaking@hotmail.co.jp>
2024-02-26 22:28:19 +0900

    row button6

--- BookManagerWinForm/ListDisplay.cs
+++ BookManagerWinForm/ListDisplay.cs
@@ -10,7 +10,7 @@ namespace BookManagerWinForm
         private bool isEditor;
         private MainMenu mainMenuForm;
         private DatabaseManager dbManager;
-        private string viewName = "view_all"; // ビュー名を正確に指定する
+        private string viewName = "view_all";
 
         public ListDisplay(int empNum, bool isEditor, MainMenu mainMenuForm)
         {
@@ -19,6 +19,10 @@ namespace BookManagerWinForm
             this.isEditor = isEditor;
             this.mainMenuForm = mainMenuForm;
             dbManager = new DatabaseManager(); // DatabaseManagerクラスのインスタンス化
+
+            // DataGridView の CellContentClick イベントハンドラを設定
+            dataGridView1.CellContentClick += DataGridView1_CellContentClick;
+
             LoadDataFromView();
         }
 
@@ -32,8 +36,8 @@ namespace BookManagerWinForm
             buttonColumn.DataPropertyName = "Button";
 
             // ボタンの外観をカスタマイズする
-            buttonColumn.DefaultCellStyle.BackColor = System.Drawing.Color.LightGray; // 薄い青
-            buttonColumn.DefaultCellStyle.ForeColor = System.Drawing.Color.Black; // テキストカラーを黒に設定
+            buttonColumn.DefaultCellStyle.BackColor = System.Drawing.Color.LightGray;
+            buttonColumn.DefaultCellStyle.ForeColor = System.Drawing.Color.Black;
             buttonColumn.FlatStyle = FlatStyle.Popup;
 
             this.dataGridView1.Columns.Add(buttonColumn);
@@ -41,7 +45,7 @@ namespace BookManagerWinForm
             // データを取得する処理
             DataTable data = dbManager.GetDataFromView(viewName);
 
-            // TEST列を追加し、ボタンを設定する
+            // ActionButton列を追加し、ボタンを設定する
             var button = new DataColumn("Button");
             button.DataType = typeof(string);
             data.Columns.Add(button);
@@ -78,7 +82,21 @@ namespace BookManagerWinForm
             dataGridView1.Columns[0].Visible = false;
         }
 
+        // DataGridView のセルがクリックされたときの処理
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // ボタンがクリックされたことを確認する
+            if (e.ColumnIndex == dataGridView1.Columns["アクションボタン"].Index && e.RowIndex != -1)
+            {
+                // クリックされた行の status_num の値を取得
+                int statusNum = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells["status_num"].Value);
 
+                // status_num の値をポップアップで表示
+                MessageBox.Show($"Status Num: {statusNum}", "Status Num", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        // 戻るボタンがクリックされたときの処理
         private void ButtonBack_Click(object sender, EventArgs e)
         {
             mainMenuForm.Show();
