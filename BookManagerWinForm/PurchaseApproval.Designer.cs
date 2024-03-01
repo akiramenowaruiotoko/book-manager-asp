429ae37f4234ec947c92c89896c074f2746c23dc
akiramenowaruiotoko <sawaking@hotmail.co.jp>
2024-03-01 23:19:55 +0900

    create form PurchaseApproval

---
+++ BookManagerWinForm/PurchaseApproval.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace BookManagerWinForm
+{
+    partial class PurchaseApproval
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            labelTitle = new Label();
+            buttonBackList = new Button();
+            SuspendLayout();
+            // 
+            // labelTitle
+            // 
+            labelTitle.AutoSize = true;
+            labelTitle.Font = new Font("Yu Gothic UI", 16F);
+            labelTitle.Location = new Point(12, 9);
+            labelTitle.Name = "labelTitle";
+            labelTitle.Size = new Size(302, 59);
+            labelTitle.TabIndex = 3;
+            labelTitle.Text = "Book Manager";
+            // 
+            // buttonBackList
+            // 
+            buttonBackList.Location = new Point(371, 16);
+            buttonBackList.Name = "buttonBackList";
+            buttonBackList.Size = new Size(302, 60);
+            buttonBackList.TabIndex = 4;
+            buttonBackList.Text = "back";
+            buttonBackList.UseVisualStyleBackColor = true;
+            // 
+            // PurchaseApproval
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(buttonBackList);
+            Controls.Add(labelTitle);
+            Name = "PurchaseApproval";
+            Text = "PurchaseApproval";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label labelTitle;
+        private Button buttonBackList;
+    }
+}
