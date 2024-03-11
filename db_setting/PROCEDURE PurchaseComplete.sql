8ab8c12939d24852386085eed067e7cbff608711
akiramenowaruiotoko <sawaking@hotmail.co.jp>
2024-03-11 19:09:36 +0900

    modify not exist status_num <> 0

--- db_setting/PROCEDURE PurchaseComplete.sql
+++ db_setting/PROCEDURE PurchaseComplete.sql
@@ -3,22 +3,22 @@ GO
 
 CREATE OR ALTER PROCEDURE [dbo].[PurchaseComplete]
     @BookId CHAR(10),
-	@EmployeeNumber INT,
+    @EmployeeNumber INT,
     @StatusNum INT
 AS
 BEGIN
     SET NOCOUNT ON;
 
-    -- 書籍が存在し、ステータスが０か確認
-    IF EXISTS (SELECT 1 FROM statuses WHERE book_id = @BookId and status_num = 0)
-		BEGIN
-			-- 書籍が存在しステータスが０の場合はstatusesテーブルを更新、RETURN 0を返す
-			INSERT INTO statuses (book_id, employee_number, status_num) VALUES (@BookId, @EmployeeNumber, @StatusNum);
-			RETURN 0;
-		END
+    -- 書籍が存在し、ステータスが０のみか確認
+    IF NOT EXISTS (SELECT 1 FROM statuses WHERE book_id = @BookId AND status_num <> 0)
+    BEGIN
+        -- 書籍が存在し、ステータスが０のみの場合はstatusesテーブルに新しいレコードを挿入し、RETURN 0を返す
+        INSERT INTO statuses (book_id, employee_number, status_num) VALUES (@BookId, @EmployeeNumber, @StatusNum);
+        RETURN 0;
+    END
     ELSE
-		BEGIN
-			-- 書籍が存在しない場合は中止、RETURN -1を返す
-			RETURN -1;
-		END
+    BEGIN
+        -- ステータスが０以外のレコードが存在する場合は中止し、RETURN -1を返す
+        RETURN -1;
+    END
 END;
